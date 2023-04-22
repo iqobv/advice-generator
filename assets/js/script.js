3021d1365bb004528951ed6fa9b8c7788d6c2455
Vitaliy <99617587+iqobv@users.noreply.github.com>
2023-04-22 23:06:42 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const btnRandomAdvice = document.querySelector(".random-advice"),
+  adviceId = document.querySelector(".advice-id"),
+  advice = document.querySelector(".advice"),
+  body = document.body;
+
+function randomAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((json) => {
+      adviceId.textContent = json.slip.id;
+      advice.textContent = json.slip.advice;
+    });
+}
